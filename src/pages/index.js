d2a03db1ec2e1049269ed2efdd16dfe2597f5dc5
Chris Dunne <chrisdunne66@gmail.com>
2021-09-27 18:14:19 +0100

    Earth2 - V0.2 - Fixed Image In place
    Bare structure of the website in place. Had to drop css modules because it wasn't really working for me and how I like to play around with css. Once I get the actual thing where I want then I can look to utilise more of gatsby has to offer.

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,60 @@ import { StaticImage } from 'gatsby-plugin-image'
 // components
 import Layout from '../components/layout'
 
+import './index.css'
+
 const IndexPage = () => {
   return (
-    <main>
+    <main className="mainWrapper" >
 
-    <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+    {/* <Layout pageTitle="Home Page">
+      <p  className={test}>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage 
         alt="earth2"
         src="https://images.pexels.com/photos/796206/pexels-photo-796206.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
       />
-    </Layout>
+    </Layout> */}
+
+{/* Navbar */}
+    <div className="navBarWrapper">
+      <nav>
+        <h1>Navbar</h1>
+        <h1>Navbar</h1>
+      </nav>
+    </div>
+
+
+{/* Fixed Image Area */}
+    <div className="backgroundWrapper">
+      <div className="backgroundImage">
+       <span className="testContent">Image</span>
+      </div>
+    </div>
+
+
+{/* Scrolly Overlap Bit */}
+    <div className="bodyWrapper">
+
+      <div className="bodyContent">
+          <h1>Content</h1>
+          <h1>Content</h1>
+          <h1>Content</h1>
+          <h1>Content</h1>
+      </div>
+
+      <div className="contactFormWrapper">
+        <h1>Contact Form</h1>
+      </div>
+
+      <div className="footerWrapper">
+        <div className="footerContent">
+          <h1>Footer 1</h1>
+          <h1>Footer 2</h1>
+        </div>
+      </div>
+    </div>
+
+
 
     </main>
   )
