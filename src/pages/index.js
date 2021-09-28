cbe8235531f2bab79bc4b7d810aee68505c6c042
Chris Dunne <chrisdunne66@gmail.com>
2021-09-28 17:00:10 +0100

    - V0.7 - Added animation to main image
    and other small tweaks

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,11 +49,16 @@ const IndexPage = () => {
         <div className="bodyContentAbout">
           <p>The Earth 2 project is about endless posibilities. It's about bold new horizons and opportunities for all mankind. Let's face it, Earth 1 had a good run but it's time to move on. Earth 2 will allow us to break free from the chains than bind us and launch us towards a brave new vision of what it means to be alive in the 22nd century and beyond.</p>
 
-          <p>The first of us are already on the way and you can be a part of that too. Everyone on earth will be allocated a spot on the arks, but you can get access ahead of the curve by registering your interest now. All you need to do is act now because it's time to go. We need to leave. There isn't much time. Hurry.</p>
         </div>
 
         
         <div className="bodyContentImage" id="upperImage"><img src={eclipse} /></div>
+
+        <div className="bodyContentAbout">
+          <p>The first of us are already on the way and you can be a part of that too. Everyone on Earth 1 will be allocated a spot on the arks, but you can get access ahead of the curve by registering your interest now. All you need to do is act now because it's time to go. We need to leave. There isn't much time. Hurry.</p>
+        </div>
+
+
         <div className="bodyContentImage" id="lowerImage"><img src={shuttle} /></div>
 
         <div className="bodyContentLatest">
@@ -228,7 +233,7 @@ const IndexPage = () => {
           <div id="footerRight">
             <p>For more information: <br/> <span>earth2@human.gov</span></p>
           </div>
-          
+
         </div>
 
         
